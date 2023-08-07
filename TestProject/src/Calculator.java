e82790c4b803a7057576172d55d20c9871949967
HP <HP@DESKTOP-6J8SA0Q>
2023-08-07 15:14:13 +0530

    second commit

---
+++ TestProject/src/Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator {
+	
+	int n1=10,n2=20;
+	public void add() {
+		System.out.println("sum is :"+n1+n2);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calculator=new Calculator();
+		calculator.add();
+
+	}
+
+}
